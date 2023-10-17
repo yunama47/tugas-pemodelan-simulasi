111b6a05437ffa09ef3ed6a01ee8ea2ee3fbc291
yunama47 <yunama1236@gmail.com>
2023-10-17 10:55:25 +0800

    update code simulasi antrean

--- simulasi_antrean.py
+++ simulasi_antrean.py
@@ -1,5 +1,5 @@
-import builtins
 import random
+import time
 
 
 def avg_distribution(distribution: dict):
@@ -31,11 +31,33 @@ def distribute_random(ran: float, cumulative: dict):
     raise ValueError("nilai random melebihi 1!")
 
 
+class Customer:
+    servicebegin = None
+    arrivaltime: int = -1
+    status: int = -1 #1: dilayani 2:ngantre 3:selesai
+    def __init__(self, name, srtime) -> None:
+        self.servicetime = srtime
+        self.timeleft = srtime
+        self.queuetime = 0
+        self.name = name
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+
 class Server:
+    current_customer: Customer = None
+    busytime: int = 0
+    idletime: int = 0
+    customercount: int = 0
+    utility: float = 0.0
+    name: str = ""
     def __init__(self) -> None:
         self.busy = False
         self.queue = []
-        self.current_customer = None
         self.servered_customers = []
 
     def set_busy(self, b):
@@ -45,36 +67,39 @@ class Server:
         self.queue.append(customer)
 
     def dequeue(self):
-        return self.queue.pop(0)
+        nextCustomer: Customer = self.queue.pop(0)
+        return nextCustomer
 
-    def update(self):
+    def update(self, function):
         if self.busy:
-            self.curent_customer.servicetime -= 1
-            if self.curent_customer.servicetime == 0:
+            self.current_customer.timeleft -= 1
+            if self.current_customer.timeleft == 0:
                 self.set_busy(False)
-                self.servered_customers.append(self.curent_customer)
-                self.curent_customer = None
+                doneCustomer = self.current_customer
+                self.current_customer = None
+                self.servered_customers.append(doneCustomer)
+                function(f"DN!{doneCustomer} done | queue = {self.queue}")
+            else:
+                function(
+                    f"IDL!current customer = {self.current_customer} | time left = {self.current_customer.timeleft} | queue = {self.queue}")
 
         if self.queue:
             for customer in self.queue:
                 customer.queuetime += 1
-
-
-class Customer:
-    def __init__(self, no_customer, srtime) -> None:
-        self.servicetime = srtime
-        self.queuetime = 0
-        self.name = f"customer {no_customer}"
-
-    def __str__(self) -> str:
-        return self.name
+        # function(
+        #     f"=================================\ncurrent = {self.current_customer}, busy = {self.busy}, queue = {self.queue}\n== debug [end] ==")
 
 
 class Simulation:
-    distribution: dict = {1: 0.1, 2: 0.2, 3: 0.3, 4: 0.25, 5: 0.1, 6: 0.05}
+    # distribution: dict = {1: 0.1, 2: 0.2, 3: 0.3, 4: 0.25, 5: 0.1, 6: 0.05}
+    distribution: dict = {7: 0.1, 3: 0.2, 9: 0.3, 8: 0.25, 6: 0.1, 2: 0.05}
     max_minute: int = 100
-    function = print
-    server: Server = Server()
+    function = lambda self, text: print(text.split("!")[-1])
+    delay: float = 0.0
+    server: Server = None
+    idle_time: int = 0
+    def create_server(self):
+        self.server = Server()
 
     def single_server_simulation(self):
         service_time_dist = self.distribution
@@ -88,46 +113,48 @@ class Simulation:
         no_customer = 1
 
         for minute in range(max_minute):
-            function(f"=== minute {minute} ===")
+            function(f"IDL!=== minute {minute} ===")
+            # function(
+            #     f"== debug [start] ==\ncurrent = {server.current_customer}, busy = {server.busy}, queue = {server.queue}\n=================================")
             if minute == next_arival:
+            #while minute == (next_arival+interarivals.pop(0)):
                 if not server.busy and not server.queue:
                     service_time = distribute_random(random.random(), cumulative)
-                    service_done = minute + service_time
-                    server.curent_customer = Customer(no_customer, service_time)
+                    service_done = minute + service_time - 1
+                    server.current_customer = Customer(f"Customer {no_customer}", service_time)
                     server.set_busy(True)
                     function(
-                        f"{server.curent_customer} arrive | service time = {service_time} | done at minute {service_done}")
+                        f"CRI!{server.current_customer} arrive | service time = {service_time} | done at minute {service_done}")
 
                 elif server.busy:
                     service_time = distribute_random(random.random(), cumulative)
-                    new_customer = Customer(no_customer, service_time)
+                    new_customer = Customer(f"Customer {no_customer}", service_time)
                     server.enqueue(new_customer)
-                    function(f"{new_customer} arrive | service time = {service_time} | enqueued")
+                    function(f"CRQ!{new_customer} arrive | service time = {service_time} | enqueued")
 
                 next_arival += interarivals.pop(0)
                 no_customer += 1
 
-            elif not server.busy and server.queue:
+            if not server.busy and server.queue:
                 next_customer = server.dequeue()
-                server.curent_customer = next_customer
+                server.current_customer = next_customer
                 server.set_busy(True)
-                function(f"{next_customer} dequeued")
+                function(f"DEQ!{next_customer} dequeued")
 
-            if server.busy:
-                function(
-                    f"current customer = {server.curent_customer} | time left = {server.curent_customer.servicetime} | length queue = {len(server.queue)}")
-            else:
-                function(f"server idle... next arrival in minute {next_arival}")
+            elif not server.busy and not server.queue:
+                function(f"IDL!server idle... next arrival in minute {next_arival}")
+                self.idle_time += 1
 
-            server.update()
-            function("\n")
+            server.update(function)
+            function("IDL!\n")
+            time.sleep(self.delay)
 
     def __init__(self, **kwargs) -> None:
         """
-        parameter =
-        distribution : distribusi service time
-        max_minute : menit maksimal system berjalan
-        function : fungsi yang akan digunakan untuk menampilkan progress
+        class untuk simulasi
+        :param distribution: distribusi service time
+        :param max_minute: menit maksimal system berjalan
+        :param function: fungsi yang akan digunakan untuk menampilkan progress
         """
         if kwargs.get("distribusi"):
             self.distribution = kwargs["distribusi"]
@@ -135,9 +162,7 @@ class Simulation:
             self.max_minute = kwargs["max_minute"]
         if kwargs.get("function"):
             self.function = kwargs["function"]
+        if kwargs.get("delay"):
+            self.delay = kwargs["delay"]
 
-
-if __name__ == "__main__":
-    a = {3: 0.19, 5: 0.2, 6: 0.3, 7: 0.3, 10: 0.01}
-    print(count_cumulative(a))
-    # print(count_cumulative(a))
+        self.create_server()
